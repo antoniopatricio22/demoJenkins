0091a2be8da9139c9803c8382120b74b2042dfd9
Antonio Macena <antonio.patricio@souunit.com.br>
2025-05-25 14:16:11 -0300

    upload: Versão V3 - testes CT04-OK

--- src/main/java/com/exemplo/Usuario.java
+++ src/main/java/com/exemplo/Usuario.java
@@ -1,23 +1,107 @@
 package com.exemplo;
 
 public class Usuario {
-    private String nome;
-    private int idade;
 
-    public Usuario(String nome, int idade) {
+    private static volatile int ultimoId = 0;
+    private int idPessoa;
+    private String nome;
+    private String login;
+    private String senha;
+    private boolean status;
+
+    public Usuario(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("O nome do usuário está incorreto.");
+        }
+        this.idPessoa = ++ultimoId;
         this.nome = nome;
-        this.idade = idade;
+        this.login = geraLogin(nome);
+        this.senha = criptografaSenha("abc123");
+        this.status = false;
+    }
+
+    public Usuario() {
+    }
+
+    public boolean alteraStatus(boolean status) {
+        this.status = status;
+        return this.status;
+    }
+
+    public String geraLogin(String nome) {
+        if (nome.isEmpty()) {
+            throw new IllegalArgumentException("O nome do usuário está incorreto.");
+        }
+        String[] partesNome = nome.split(" ");
+        return partesNome[0].toLowerCase() + "." + partesNome[partesNome.length - 1].toLowerCase();
+    }
+
+    private String criptografaSenha(String senha) {
+        StringBuilder senhaCriptografada = new StringBuilder();
+        for (int i = senha.length() - 1; i >= 0; i--) {
+            char c = (char) (senha.charAt(i) - 10);
+            senhaCriptografada.append(c);
+        }
+        return senhaCriptografada.toString();
+    }
+
+    public boolean autentica(String login, String senha) {
+        if (!status) {
+            return false;
+        }
+        return login.equals(this.login) && criptografaSenha(senha).equals(this.senha);
+    }
+
+    public boolean alteraSenha(String login, String senhaAtual, String novaSenha, String confirmaSenha) {
+        if (autentica(login, senhaAtual)) {
+            if (!validaSenha(novaSenha)) {
+                throw new IllegalArgumentException("Nova senha fora do padrão.");
+            }
+            if (!novaSenha.equals(confirmaSenha)) {
+                throw new IllegalArgumentException("Nova senha não confere com a senha de confirmação.");
+            }
+            this.senha = criptografaSenha(novaSenha);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validaSenha(String senha) {
+        boolean temLetra = false, temNumero = false;
+        for (char c : senha.toCharArray()) {
+            if (Character.isLetter(c)) temLetra = true;
+            if (Character.isDigit(c)) temNumero = true;
+        }
+        return temLetra && temNumero;
+    }
+
+    public int getIdPessoa() {
+        return idPessoa;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public int getIdade() {
-        return idade;
+    public String getLogin() {
+        return login;
     }
 
-    public boolean isMaiorDeIdade() {
-        return idade >= 18;
+    public String getSenha() {
+        return senha;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:\n" +
+                "idPessoa: " + idPessoa + "\n" +
+                "Nome    : " + nome + "\n" +
+                "Login   : " + login + "\n" +
+                "Ativo   : " + status;
     }
 }
+
--- src/test/java/com/exemplo/UsuarioTest.java
+++ src/test/java/com/exemplo/UsuarioTest.java
@@ -1,21 +1,100 @@
 package com.exemplo;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class UsuarioTest {
+/**
+ *
+ * @author Equipe 02
+ */
 
-    @Test
-    public void testUsuarioMaiorDeIdade() {
-        Usuario usuario = new Usuario("João", 20);
-        assertTrue(usuario.isMaiorDeIdade());
+class UsuarioTest {
+    private Usuario usuarioAtivo;
+    private Usuario usuarioDesativado;
+
+    @BeforeEach
+    void setUp() {
+        usuarioAtivo = new Usuario("Fulano Fulano");
+        //Ativando usuário para teste
+        usuarioAtivo.alteraStatus(true);
+        
+        usuarioDesativado = new Usuario("Beltrano Beltrano");
     }
 
     @Test
-    public void testUsuarioMenorDeIdade() {
-        Usuario usuario = new Usuario("Maria", 15);
-        assertFalse(usuario.isMaiorDeIdade());
+    @DisplayName("CT01: Autenticação de um usuário ativo com login e senha válidos")
+    void testAutenticaCT01() {
+        // Cenário
+        String login = "fulano.fulano";
+        String senha = "abc123";
+        boolean resultadoEsperado = true;
+
+        // Execução
+        boolean resultadoAtual = usuarioAtivo.autentica(login, senha);
+
+        // Verificação
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    @DisplayName("CT02: Autenticação de um usuário ativo com login inválido e senha válida")
+    void testAutenticaCT02() {
+        // Cenário
+        String login = "fulano.beltrano";
+        String senha = "abc123";
+        boolean resultadoEsperado = false;
+
+        // Execução
+        boolean resultadoAtual = usuarioAtivo.autentica(login, senha);
+
+        // Verificação
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    @DisplayName("CT03: Autenticação de um usuário ativo com login válido e senha inválida")
+    void testAutenticaCT03() {
+        // Cenário
+        String login = "fulano.fulano";
+        String senha = "teste";
+        boolean resultadoEsperado = false;
+
+        // Execução
+        boolean resultadoAtual = usuarioAtivo.autentica(login, senha);
+
+        // Verificação
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    @DisplayName("CT04: Autenticação de um usuário desativado com login e senha válidos")
+    void testAutenticaCT04() {
+        // Cenário
+        String login = "beltrano.beltrano";
+        String senha = "abc123";
+        boolean resultadoEsperado = false;
+
+        // Execução
+        boolean resultadoAtual = usuarioDesativado.autentica(login, senha);
+
+        // Verificação
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    @DisplayName("CT05: Autenticação de um usuário desativado com login inválido e senha inválida")
+    void testAutenticaCT05() {
+        // Cenário
+        String login = "beltrano.fulano";
+        String senha = "teste";
+        boolean resultadoEsperado = false;
+
+        // Execução
+        boolean resultadoAtual = usuarioDesativado.autentica(login, senha);
+
+        // Verificação
+        assertEquals(resultadoEsperado, resultadoAtual);
     }
 }
